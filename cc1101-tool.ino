f8d1c25d636c64d3fd1d0b997009596298c64103
Adam Loboda <43117081+mcore1976@users.noreply.github.com>
2023-05-29 17:57:54 +0200

    Update cc1101-tool.ino

--- cc1101-tool.ino
+++ cc1101-tool.ino
@@ -102,38 +102,38 @@ static void exec(char *cmdline)
       
     if (strcmp_P(command, PSTR("help")) == 0) {
         Serial.println(F(
-          "setModulation <mode>         // set modulation mode. 0 = 2-FSK, 1 = GFSK, 2 = ASK/OOK, 3 = 4-FSK, 4 = MSK.\r\n\r\n"
-          "setMHZ <frequency>           // Here you can set your basic frequency. default = 433.92).The cc1101 can: 300-348 MHZ, 387-464MHZ and 779-928MHZ.\r\n\r\n"
-          "setDeviation <deviation>     // Set the Frequency deviation in kHz. Value from 1.58 to 380.85. Default is 47.60 kHz.\r\n\r\n"
-          "setChannel <channel>         // Set the Channelnumber from 0 to 255. Default is cahnnel 0.\r\n\r\n"
+          "setmodulation <mode>         // set modulation mode. 0 = 2-FSK, 1 = GFSK, 2 = ASK/OOK, 3 = 4-FSK, 4 = MSK.\r\n\r\n"
+          "setmhz <frequency>           // Here you can set your basic frequency. default = 433.92).The cc1101 can: 300-348 MHZ, 387-464MHZ and 779-928MHZ.\r\n\r\n"
+          "setdeviation <deviation>     // Set the Frequency deviation in kHz. Value from 1.58 to 380.85. Default is 47.60 kHz.\r\n\r\n"
+          "setchannel <channel>         // Set the Channelnumber from 0 to 255. Default is cahnnel 0.\r\n\r\n"
          ));
         Serial.println(F(
-          "setChsp <spacing>            // The channel spacing is multiplied by the channel number CHAN and added to the base frequency in kHz. Value from 25.39 to 405.45. Default is 199.95 kHz. \r\n\r\n"
-          "setRxBW <Receive bandwidh>   // Set the Receive Bandwidth in kHz. Value from 58.03 to 812.50. Default is 812.50 kHz.\r\n\r\n"
-          "setDRate <datarate>          // Set the Data Rate in kBaud. Value from 0.02 to 1621.83. Default is 99.97 kBaud!\r\n\r\n"
-          "setPA <power value>          // Set TxPower. The following settings are possible depending on the frequency band.  (-30  -20  -15  -10  -6    0    5    7    10   11   12) Default is max!\r\n\r\n"
-          "setSyncMode  <sync mode>     // Combined sync-word qualifier mode. 0 = No preamble/sync. 1 = 16 sync word bits detected. 2 = 16/16 sync word bits detected. 3 = 30/32 sync word bits detected. 4 = No preamble/sync, carrier-sense above threshold. 5 = 15/16 + carrier-sense above threshold. 6 = 16/16 + carrier-sense above threshold. 7 = 30/32 + carrier-sense above threshold.\r\n\r\n"
-          "setSyncWord <LOW, HIGH>      // Set sync word. Must be the same for the transmitter and receiver. (Syncword high, Syncword low)\r\n\r\n"
+          "setchsp <spacing>            // The channel spacing is multiplied by the channel number CHAN and added to the base frequency in kHz. Value from 25.39 to 405.45. Default is 199.95 kHz. \r\n\r\n"
+          "setrxbw <Receive bandwidh>   // Set the Receive Bandwidth in kHz. Value from 58.03 to 812.50. Default is 812.50 kHz.\r\n\r\n"
+          "setdrate <datarate>          // Set the Data Rate in kBaud. Value from 0.02 to 1621.83. Default is 99.97 kBaud!\r\n\r\n"
+          "setpa <power value>          // Set TxPower. The following settings are possible depending on the frequency band.  (-30  -20  -15  -10  -6    0    5    7    10   11   12) Default is max!\r\n\r\n"
+          "setsyncmode  <sync mode>     // Combined sync-word qualifier mode. 0 = No preamble/sync. 1 = 16 sync word bits detected. 2 = 16/16 sync word bits detected. 3 = 30/32 sync word bits detected. 4 = No preamble/sync, carrier-sense above threshold. 5 = 15/16 + carrier-sense above threshold. 6 = 16/16 + carrier-sense above threshold. 7 = 30/32 + carrier-sense above threshold.\r\n\r\n"
+          "setsyncword <LOW, HIGH>      // Set sync word. Must be the same for the transmitter and receiver. (Syncword high, Syncword low)\r\n\r\n"
          ));
         Serial.println(F(
-          "setAdrChk <address check>    // Controls address check configuration of received packages. 0 = No address check. 1 = Address check, no broadcast. 2 = Address check and 0 (0x00) broadcast. 3 = Address check and 0 (0x00) and 255 (0xFF) broadcast.\r\n\r\n"
-          "setAddr <address>            // Address used for packet filtration. Optional broadcast addresses are 0 (0x00) and 255 (0xFF).\r\n\r\n"
-          "setWhiteData <whitening>     // Turn data whitening on / off. 0 = Whitening off. 1 = Whitening on.\r\n\r\n"
-          "setPktFormat <pkt format>    // Format of RX and TX data. 0 = Normal mode, use FIFOs for RX and TX. 1 = Synchronous serial mode, Data in on GDO0 and data out on either of the GDOx pins. 2 = Random TX mode; sends random data using PN9 generator. Used for test. Works as normal mode, setting 0 (00), in RX. 3 = Asynchronous serial mode, Data in on GDO0 and data out on either of the GDOx pins.\r\n\r\n"
-          "setLengthConfig <mode>       // 0 = Fixed packet length mode. 1 = Variable packet length mode. 2 = Infinite packet length mode. 3 = Reserved \r\n\r\n"
-          "setPacketLength <mode>       // Indicates the packet length when fixed packet length mode is enabled. If variable packet length mode is used, this value indicates the maximum packet length allowed.\r\n\r\n"
+          "setadrchk <address check>    // Controls address check configuration of received packages. 0 = No address check. 1 = Address check, no broadcast. 2 = Address check and 0 (0x00) broadcast. 3 = Address check and 0 (0x00) and 255 (0xFF) broadcast.\r\n\r\n"
+          "setaddr <address>            // Address used for packet filtration. Optional broadcast addresses are 0 (0x00) and 255 (0xFF).\r\n\r\n"
+          "setwhitedata <whitening>     // Turn data whitening on / off. 0 = Whitening off. 1 = Whitening on.\r\n\r\n"
+          "setpktformat <pkt format>    // Format of RX and TX data. 0 = Normal mode, use FIFOs for RX and TX. 1 = Synchronous serial mode, Data in on GDO0 and data out on either of the GDOx pins. 2 = Random TX mode; sends random data using PN9 generator. Used for test. Works as normal mode, setting 0 (00), in RX. 3 = Asynchronous serial mode, Data in on GDO0 and data out on either of the GDOx pins.\r\n\r\n"
+          "setlengthconfig <mode>       // 0 = Fixed packet length mode. 1 = Variable packet length mode. 2 = Infinite packet length mode. 3 = Reserved \r\n\r\n"
+          "setpacketlength <mode>       // Indicates the packet length when fixed packet length mode is enabled. If variable packet length mode is used, this value indicates the maximum packet length allowed.\r\n\r\n"
          ));
         Serial.println(F(
-          "setCrc <mode>                // 1 = CRC calculation in TX and CRC check in RX enabled. 0 = CRC disabled for TX and RX.\r\n\r\n"
-          "setCRC_AF <mode>             // Enable automatic flush of RX FIFO when CRC is not OK. This requires that only one packet is in the RXIFIFO and that packet length is limited to the RX FIFO size.\r\n\r\n"
-          "setDcFilterOff <mode>        // Disable digital DC blocking filter before demodulator. Only for data rates ≤ 250 kBaud The recommended IF frequency changes when the DC blocking is disabled. 1 = Disable (current optimized). 0 = Enable (better sensitivity).\r\n\r\n"
-          "setManchester <mode>         // Enables Manchester encoding/decoding. 0 = Disable. 1 = Enable.\r\n\r\n"
-          "setFEC <mode>                // Enable Forward Error Correction (FEC) with interleaving for packet payload (Only supported for fixed packet length mode. 0 = Disable. 1 = Enable.\r\n\r\n"
-          "setPRE <mode>                // Sets the minimum number of preamble bytes to be transmitted. Values: 0 : 2, 1 : 3, 2 : 4, 3 : 6, 4 : 8, 5 : 12, 6 : 16, 7 : 24\r\n\r\n"
-          "setPQT <mode>                // Preamble quality estimator threshold. The preamble quality estimator increases an internal counter by one each time a bit is received that is different from the previous bit, and decreases the counter by 8 each time a bit is received that is the same as the last bit. A threshold of 4∙PQT for this counter is used to gate sync word detection. When PQT=0 a sync word is always accepted.\r\n\r\n"
+          "setcrc <mode>                // 1 = CRC calculation in TX and CRC check in RX enabled. 0 = CRC disabled for TX and RX.\r\n\r\n"
+          "setcrcaf <mode>             // Enable automatic flush of RX FIFO when CRC is not OK. This requires that only one packet is in the RXIFIFO and that packet length is limited to the RX FIFO size.\r\n\r\n"
+          "setdcfilteroff <mode>        // Disable digital DC blocking filter before demodulator. Only for data rates ≤ 250 kBaud The recommended IF frequency changes when the DC blocking is disabled. 1 = Disable (current optimized). 0 = Enable (better sensitivity).\r\n\r\n"
+          "setmanchester <mode>         // Enables Manchester encoding/decoding. 0 = Disable. 1 = Enable.\r\n\r\n"
+          "setfec <mode>                // Enable Forward Error Correction (FEC) with interleaving for packet payload (Only supported for fixed packet length mode. 0 = Disable. 1 = Enable.\r\n\r\n"
+          "setpre <mode>                // Sets the minimum number of preamble bytes to be transmitted. Values: 0 : 2, 1 : 3, 2 : 4, 3 : 6, 4 : 8, 5 : 12, 6 : 16, 7 : 24\r\n\r\n"
+          "setpqt <mode>                // Preamble quality estimator threshold. The preamble quality estimator increases an internal counter by one each time a bit is received that is different from the previous bit, and decreases the counter by 8 each time a bit is received that is the same as the last bit. A threshold of 4∙PQT for this counter is used to gate sync word detection. When PQT=0 a sync word is always accepted.\r\n\r\n"
            ));
         Serial.println(F(
-       "setAppendStatus <mode>       // When enabled, two status bytes will be appended to the payload of the packet. The status bytes contain RSSI and LQI values, as well as CRC OK.\r\n\r\n"
+         "setappendstatus <mode>       // When enabled, two status bytes will be appended to the payload of the packet. The status bytes contain RSSI and LQI values, as well as CRC OK.\r\n\r\n"
          "receive <mode>               // Enable or disable printing of received RF packets on serial terminal. 1 = enabled, 0 = disabled\r\n\r\n"
          "transmit <times> <hex-vals>  // Send the same packet of 64 hex values over RF \r\n\r\n"
          "jamming <mode>               // Enable or disable continous jamming on selected band. 1 = enabled, 0 = disabled\r\n\r\n"
@@ -142,70 +142,70 @@ static void exec(char *cmdline)
          "echo <mode>                  // Enable or disable Echo on serial terminal. 1 = enabled, 0 = disabled\r\n\r\n"
          ));
             
-    } else if (strcmp_P(command, PSTR("setModulation")) == 0) {
+    } else if (strcmp_P(command, PSTR("setmodulation")) == 0) {
         int modulation = atoi(cmdline);
         ELECHOUSE_cc1101.setModulation(modulation);
         Serial.print("\r\nModulation set to ");
         Serial.print(modulation);
         Serial.print(" \r\n");
         
-    } else if (strcmp_P(command, PSTR("setMHZ")) == 0) {
+    } else if (strcmp_P(command, PSTR("setmhz")) == 0) {
         float frequency = atof(cmdline);
         ELECHOUSE_cc1101.setMHZ(frequency);
         Serial.print("\r\nFrequency set to ");
         Serial.print(frequency);
         Serial.print(" MHz \r\n");
         
-    } else if (strcmp_P(command, PSTR("setDeviation")) == 0) {
+    } else if (strcmp_P(command, PSTR("setdeviation")) == 0) {
         float deviation = atof(cmdline);
         ELECHOUSE_cc1101.setDeviation(deviation);
         Serial.print("\r\nDeviation set to ");
         Serial.print(deviation);
         Serial.print(" KHz \r\n");        
         
-    } else if (strcmp_P(command, PSTR("setChannel")) == 0) {
+    } else if (strcmp_P(command, PSTR("setchannel")) == 0) {
         int channelnumber = atoi(cmdline);
         ELECHOUSE_cc1101.setChannel(channelnumber);
         Serial.print("\r\nChannel number set to ");
         Serial.print(channelnumber);
         Serial.print("\r\n");        
         
-    } else if (strcmp_P(command, PSTR("setChsp")) == 0) {
+    } else if (strcmp_P(command, PSTR("setchsp")) == 0) {
         float spacing = atof(cmdline);
         ELECHOUSE_cc1101.setChsp(spacing);
         Serial.print("\r\nChannel spacing set to ");
         Serial.print(spacing);
         Serial.print(" kHz \r\n");  
         
-    } else if (strcmp_P(command, PSTR("setRxBW")) == 0) {
+    } else if (strcmp_P(command, PSTR("setrxbw")) == 0) {
         float rxbandwidth = atof(cmdline);
         ELECHOUSE_cc1101.setRxBW(rxbandwidth);
         Serial.print("\r\nReceive bandwidth set to ");
         Serial.print(rxbandwidth);
         Serial.print(" kHz \r\n");  
         
-    } else if (strcmp_P(command, PSTR("setDRate")) == 0) {
+    } else if (strcmp_P(command, PSTR("setdrate")) == 0) {
         float datarate = atof(cmdline);
         ELECHOUSE_cc1101.setDRate(datarate);
         Serial.print("\r\nData rate set to ");
         Serial.print(datarate);
         Serial.print(" kilo bauds \r\n");  
 
-    } else if (strcmp_P(command, PSTR("setPA")) == 0) {
+    } else if (strcmp_P(command, PSTR("setpa")) == 0) {
         int txpower = atoi(cmdline);
         ELECHOUSE_cc1101.setPA(txpower);
         Serial.print("\r\nTransmission power set to ");
         Serial.print(txpower);
         Serial.print(" \r\n");  
         
-    } else if (strcmp_P(command, PSTR("setSyncMode")) == 0) {
+    } else if (strcmp_P(command, PSTR("setsyncmode")) == 0) {
         int syncmode = atoi(cmdline);
         ELECHOUSE_cc1101.setSyncMode(syncmode);
         Serial.print("\r\nSynchronization mode set to ");
         Serial.print(syncmode);
         Serial.print(" \r\n");  
         
-    } else if (strcmp_P(command, PSTR("setSyncWord")) == 0) {
+    } else if (strcmp_P(command, PSTR("setsyncword")) == 0) {
         int lowword = atoi(strsep(&cmdline, " "));
         int highword = atoi(cmdline);
         ELECHOUSE_cc1101.setSyncWord(lowword, highword);
@@ -216,98 +216,98 @@ static void exec(char *cmdline)
         Serial.print(lowword);
         Serial.print(" \r\n");  
 
-    } else if (strcmp_P(command, PSTR("setAdrChk")) == 0) {
+    } else if (strcmp_P(command, PSTR("setadrchk")) == 0) {
         int adrcheck = atoi(cmdline);
         ELECHOUSE_cc1101.setAdrChk(adrcheck);
         Serial.print("\r\nAddress checking mode set to ");
         Serial.print(adrcheck);
         Serial.print(" \r\n");  
         
-    } else if (strcmp_P(command, PSTR("setAddr")) == 0) {
+    } else if (strcmp_P(command, PSTR("setaddr")) == 0) {
         int address = atoi(cmdline);
         ELECHOUSE_cc1101.setAddr(address);
         Serial.print("\r\nAddress set to ");
         Serial.print(address);
         Serial.print(" \r\n");  
 
-    } else if (strcmp_P(command, PSTR("setWhiteData")) == 0) {
+    } else if (strcmp_P(command, PSTR("setwhitedata")) == 0) {
         int whitening = atoi(cmdline);
         ELECHOUSE_cc1101.setWhiteData(whitening);
         Serial.print("\r\nWhitening configuration set to ");
         Serial.print(whitening);
         Serial.print(" \r\n");  
         
-    } else if (strcmp_P(command, PSTR("setPktFormat")) == 0) {
+    } else if (strcmp_P(command, PSTR("setpktformat")) == 0) {
         int pktformat = atoi(cmdline);
         ELECHOUSE_cc1101.setPktFormat(pktformat);
         Serial.print("\r\nPacket format set to ");
         Serial.print(pktformat);
         Serial.print(" \r\n");  
   
-    } else if (strcmp_P(command, PSTR("setLengthConfig")) == 0) {
+    } else if (strcmp_P(command, PSTR("setlengthconfig")) == 0) {
         int lengthconfig = atoi(cmdline);
         ELECHOUSE_cc1101.setLengthConfig(lengthconfig);
         Serial.print("\r\nPacket length mode set to ");
         Serial.print(lengthconfig);
         Serial.print(" \r\n");  
   
-    } else if (strcmp_P(command, PSTR("setPacketLength")) == 0) {
+    } else if (strcmp_P(command, PSTR("setpacketlength")) == 0) {
         int pktlength = atoi(cmdline);
         ELECHOUSE_cc1101.setPacketLength(pktlength);
         Serial.print("\r\nPacket length set to ");
         Serial.print(pktlength);
         Serial.print(" \r\n");  
         
-    } else if (strcmp_P(command, PSTR("setCrc")) == 0) {
+    } else if (strcmp_P(command, PSTR("setcrc")) == 0) {
         int pktcrc = atoi(cmdline);
         ELECHOUSE_cc1101.setCrc(pktcrc);
         Serial.print("\r\nCRC setting changed to ");
         Serial.print(pktcrc);
         Serial.print(" \r\n"); 
         
-    } else if (strcmp_P(command, PSTR("setCRC_AF")) == 0) {
+    } else if (strcmp_P(command, PSTR("setcrcaf")) == 0) {
         int pktcrcaf = atoi(cmdline);
         ELECHOUSE_cc1101.setCRC_AF(pktcrcaf);
         Serial.print("\r\nCRC Autoflush setting changed to ");
         Serial.print(pktcrcaf);
         Serial.print(" \r\n"); 
         
-     } else if (strcmp_P(command, PSTR("setDcFilterOff")) == 0) {
+     } else if (strcmp_P(command, PSTR("setdcfilteroff")) == 0) {
         int dcfilter = atoi(cmdline);
         ELECHOUSE_cc1101.setDcFilterOff(dcfilter);
         Serial.print("\r\nDC filter setting changed to ");
         Serial.print(dcfilter);
         Serial.print(" \r\n"); 
 
-     } else if (strcmp_P(command, PSTR("setManchester")) == 0) {
+     } else if (strcmp_P(command, PSTR("setmanchester")) == 0) {
         int manchset = atoi(cmdline);
         ELECHOUSE_cc1101.setManchester(manchset);
         Serial.print("\r\nManchester coding setting changed to ");
         Serial.print(manchset);
         Serial.print(" \r\n"); 
 
-     } else if (strcmp_P(command, PSTR("setFEC")) == 0) {
+     } else if (strcmp_P(command, PSTR("setfec")) == 0) {
         int fecset = atoi(cmdline);
         ELECHOUSE_cc1101.setFEC(fecset);
         Serial.print("\r\nForward Error Correction setting changed to ");
         Serial.print(fecset);
         Serial.print(" \r\n"); 
 
-     } else if (strcmp_P(command, PSTR("setPRE")) == 0) {
+     } else if (strcmp_P(command, PSTR("setpre")) == 0) {
         int preambleset = atoi(cmdline);
         ELECHOUSE_cc1101.setPRE(preambleset);
         Serial.print("\r\nMinimum preamble length changed to ");
         Serial.print(preambleset);
         Serial.print(" \r\n"); 
 
-      } else if (strcmp_P(command, PSTR("setPQT")) == 0) {
+      } else if (strcmp_P(command, PSTR("setpqt")) == 0) {
         int preamblequal = atoi(cmdline);
         ELECHOUSE_cc1101.setPQT(preamblequal);
         Serial.print("\r\nPreamble quality estimator changed to ");
         Serial.print(preamblequal);
         Serial.print(" \r\n"); 
 
-       } else if (strcmp_P(command, PSTR("setAppendStatus")) == 0) {
+       } else if (strcmp_P(command, PSTR("setappendstatus")) == 0) {
         int appendstatus = atoi(cmdline);
         ELECHOUSE_cc1101.setAppendStatus(appendstatus);
         Serial.print("\r\nStatus bytes appending changed to ");
